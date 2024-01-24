d786cc269b1385f60b8c578f18d45e32f63c8dca
sannorozco <santiagorozco@gmail.com>
2024-01-24 17:22:15 -0600

    Update blends

---
+++ tools/updateBlends.py
@@ -0,0 +1,29 @@
+import ufoProcessor
+import json
+
+myPath = "../source/Parametric-avar2/RobotoAvar2-avar2.designspace"
+
+doc = ufoProcessor.DesignSpaceProcessor()
+
+doc.read(myPath)
+
+blends = {}
+
+for instance in doc.instances:
+    
+    print( '\t\t"' + instance.styleName + '": {')
+    
+    blend ={}
+     
+    for location in instance.designLocation:
+        
+        if location == "Optical size" or location == "Slant" or location == "Weight" or location == "Width":
+            pass
+        else:
+            print ('\t\t\t"' + location + '"' + ": " + str(instance.designLocation[location]) + ",")
+    
+    print("\t\t},")
+    
+#print ( blends )
+    
+#print(json.dumps(blends, indent = 4))
