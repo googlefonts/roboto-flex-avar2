d46c448cecd45b54eed8da9781e9f4a13d325aa7
Santiago Orozco <santiagorozco@gmail.com>
2024-02-14 18:57:47 -0600

    Moving fences & helper scripts
    —Made helper scripts to generate the location mapping values, so we can bring them back from the default, strange issue.
    —Added more mappings.

---
+++ tools/makeMappingLocation.py
@@ -0,0 +1,45 @@
+from fontTools.designspaceLib import DesignSpaceDocument, AxisDescriptor, SourceDescriptor, InstanceDescriptor, AxisMappingDescriptor
+import ufoProcessor
+from lxml import etree
+
+myPath = "source/Parametric-avar2/RobotoAvar2-avar1.designspace"
+
+doc = ufoProcessor.DesignSpaceProcessor()
+
+doc.read(myPath)
+
+tag_fence = "YOPQ"
+
+tag_opsz = "Optical size"
+tag_width = "Width"
+tag_weight = "Weight"
+
+fences_designspace = "tools/fences.designspace"
+
+for instance in doc.instances:
+    
+    if instance.designLocation[tag_opsz] == 144.0  and instance.designLocation[tag_width] == 100 and instance.designLocation[tag_weight] == 400:
+        
+        #print ( instance )
+        
+        m1 = AxisMappingDescriptor()
+        
+        m1.inputLocation = { tag_opsz : instance.designLocation[tag_opsz],
+        				     tag_width : instance.designLocation[tag_width],
+        				     tag_weight : instance.designLocation[tag_weight],
+        				     tag_fence : instance.designLocation[tag_fence],
+         }
+        
+        for location in instance.designLocation:
+            
+            #print ('\t\t\t"' + location + '"' + ": " + str(instance.designLocation[location]) + ",")
+            #m1.inputLocation = {k: v for k, v in instance.designLocation.items()}
+            m1.outputLocation = {k: v for k, v in instance.designLocation.items()}
+        
+        #print ( m1 )
+        
+        designspace = DesignSpaceDocument()
+        
+        designspace.addAxisMapping(m1)
+        
+        designspace.write(fences_designspace)
