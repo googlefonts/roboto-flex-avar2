0a654cdaa2785ebc82cdc1e86f7b0b6bbeab582d
Santiago Orozco <santiagorozco@gmail.com>
2024-06-04 21:33:29 -0600

    Update VFs
    —Anchors & some widths in default
    —Updated check scripts

--- tools/checkSidebearingsAll.py
+++ tools/checkSidebearingsAll.py
@@ -4,13 +4,17 @@ PATH = '../source/Parametric-avar2/'
 
 DEFAULT = 'RobotoAvar2-wght400.ufo'
 
-SOURCES = [                        'RobotoAvar2-XOPQ2.ufo',            'RobotoAvar2-XOPQ310.ufo',
-            'RobotoAvar2-XTRA244.ufo',            'RobotoAvar2-XTRA741.ufo',            'RobotoAvar2-YOPQ2.ufo',            'RobotoAvar2-YOPQ280.ufo',
+SOURCES = [                        # 'RobotoAvar2-XOPQ2.ufo',            # 'RobotoAvar2-XOPQ310.ufo',
+            # 'RobotoAvar2-XTRA244.ufo',             'RobotoAvar2-XTRA741.ufo',            # 'RobotoAvar2-YOPQ2.ufo',            # 'RobotoAvar2-YOPQ280.ufo',
             
         ]
 
 asciiGlyphs = 'space exclam quotedbl numbersign dollar percent ampersand quotesingle parenleft parenright asterisk plus comma hyphen period slash zero one two three four five six seven eight nine colon semicolon less equal greater question at A B C D E F G H I J K L M N O P Q R S T U V W X Y Z bracketleft backslash bracketright asciicircum underscore grave a b c d e f g h i j k l m n o p q r s t u v w x y z braceleft bar braceright asciitilde'.split()
-        
+
+#glyphExceptions = ['K', 'k', ]
+glyphExceptions = [ ]
+
+
 def checkSedebearings(fonts):
     
     for font in fonts:
@@ -24,15 +28,21 @@ def checkSedebearings(fonts):
         strDifferences = ''
 
 
-        for glyph in asciiGlyphs:
-    
-            if font1[glyph].leftMargin != font2[glyph].leftMargin:
-                
-                strDifferences += 'LEFT Sidebearing difference in :' + glyph + ',' + str(font1[glyph].leftMargin) + ' ' + str(font2[glyph].leftMargin) + '\n'
+        for glyph in font2.glyphOrder:
             
-            if font1[glyph].rightMargin != font2[glyph].rightMargin:
+            if ( glyph.find('comb') < 0 ) or ( glyph in glyphExceptions ):
+            
+                if font1[glyph].leftMargin != font2[glyph].leftMargin:
+                
+                    strDifferences += 'LEFT Sidebearing difference in :' + glyph + ',' + str(font1[glyph].leftMargin) + ' ' + str(font2[glyph].leftMargin) + '\n'
+                
+                    #font2[glyph].leftMargin = font1[glyph].leftMargin
+                
+                if font1[glyph].rightMargin != font2[glyph].rightMargin:
         
-                strDifferences += 'RIGHT Sidebearing difference in :' + glyph + ',' + str(font1[glyph].rightMargin) + ' ' + str(font2[glyph].rightMargin) + '\n'        
+                    strDifferences += 'RIGHT Sidebearing difference in :' + glyph + ',' + str(font1[glyph].rightMargin) + ' ' + str(font2[glyph].rightMargin) + '\n'
+                
+                    #font2[glyph].rightMargin = font1[glyph].rightMargin
                     
 
         #font2.save()
@@ -40,15 +50,16 @@ def checkSedebearings(fonts):
         if len(strDifferences) == 0:
     
             #s = NSSpeechSynthesizer.alloc().init()
-            #s.startSpeakingString_("Finished. There are no width differences in Google Sans Flex. Yay!")
+            #s.startSpeakingString_("Finished. There are no width differences in Roboto Flex AVAR2. Yay!")
             print( '🙂🙂🙂 No width differences betweeen ' + fontName1 + ' & ' +fontName2 )
         else:
             #s = NSSpeechSynthesizer.alloc().init()
-            #s.startSpeakingString_("FUCK")
+            #s.startSpeakingString_("NOOOO")
             print( '😡😡😡 Width differences betweeen ' + fontName1 + ' & ' +fontName2 )
             print(strDifferences)
         
         font1.close()
+        #font2.save()
         font2.close()
             
 
