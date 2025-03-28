412ac9c97f11e9e0c14f8ca3e9c79b76fcea1072
Santiago Orozco <santiagorozco@gmail.com>
2025-03-28 16:12:42 -0600

    Renaming to Roboto Delta

---
+++ Source/Parametric-avar2/build-Roboto-Delta.sh
@@ -0,0 +1,14 @@
+
+source tools/robotoflexA2-env/bin/activate
+
+## MAKE VF
+
+## full glyphset
+fontmake -m source/Parametric-avar2/Roboto-Delta.designspace -o variable --output-path fonts/LGCAlpha/Roboto-Delta-VF.ttf --no-production-names
+fontmake -m source/Parametric-avar2/Roboto-Delta-no-fences.designspace -o variable --output-path fonts/LGCAlpha/Roboto-Delta-no-fences-VF.ttf --no-production-names
+fontmake -m source/Parametric-avar2/Roboto-Delta-no-slant.designspace -o variable --output-path fonts/LGCAlpha/Roboto-Delta-no-slant-VF.ttf --no-production-names
+
+## MAKE INSTANCES
+#fontmake -m source/Parametric-avar2/RobotoAvar2-avar2.designspace -i -o ufo --output-dir source/Parametric-avar2/instances/
+
+deactivate
